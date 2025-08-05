aebba0bda493aa5421c35f6d6e12e215968a6d61
Temesgenf <tamasgenfiqaadu@gmail.com>
2025-08-05 17:34:34 +0300

    code refactor

--- client/src/components/layout/AppHeader.tsx
+++ client/src/components/layout/AppHeader.tsx
@@ -207,7 +207,7 @@ const AppHeader: React.FC = () => {
   };
 
   return (
-    <header className="sticky top-0 flex w-full bg-white border-gray-200 z-10 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
+    <header className="sticky top-0 flex w-full bg-white border-gray-200 z-40 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
       <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
         <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-gray-800 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
           <button
@@ -285,7 +285,7 @@ const AppHeader: React.FC = () => {
           <div className="hidden lg:block relative">
             <form>
               <div className="relative">
-                <span className="absolute -translate-y-1/2 pointer-events-none left-4 top-1/2 z-10">
+                <span className="absolute -translate-y-1/2 pointer-events-none left-4 top-1/2 z-40">
                   <svg
                     className="fill-gray-500 dark:fill-gray-400 transition-colors"
                     width="20"
